253f4665781228f70dde1f2def335e494ed689dd
Doney <doneypaul96@gmail.com>
2019-04-04 14:00:25 +0530

    sample

---
+++ recivetcpmessage.py
@@ -0,0 +1,54 @@
+
+import socket
+from MasterThread import MasterThread
+from SplitDownloader import * 
+import select
+import merge
+
+
+class recive_tcp_message:
+        def __init__(self,):
+            self.BUFFER_SIZE = 1024
+            self.list_of_param = []
+            self.threads = []
+            self.sequence = 0
+        def tcp_listen(self,tcpsock):
+            read_list = [tcpsock]
+            tcpsock.listen(1)
+            print ("Waiting for incoming connections...")
+            #while True:
+            try:
+                readable, writable, errored = select.select(read_list, [], [],1)
+                for s in readable:
+                    if s is tcpsock:
+                        (conn, (ip,port)) = tcpsock.accept()
+                        msg=conn.recv(self.BUFFER_SIZE)
+                        self.sequence = self.sequence+1
+                        print(msg,self.sequence)
+                        
+                        print("Got connection from client ",self.sequence, (ip,port))
+                        self.list_of_param.append([ip,port,conn,self.sequence])
+                    else:
+                        pass
+                            
+            except socket.timeout:
+                    pass
+        def tcp_thread(self):    
+            for i in self.list_of_param:
+                print(i)
+            #code to pass url here
+            #@TODO get url from user
+            #url='https://www.w3.org/TR/PNG/iso_8859-1.txt'
+            url='https://dlcdnets.asus.com/pub/ASUS/nb/DriversForWin8/Audio/Audio_Realtek_Win8_64_Z6016716.zip'
+            client_url=Start_split(url,self.sequence)
+            #starting threads
+            for i in range(self.sequence) :
+                newthread = MasterThread(self.list_of_param[i],client_url[i])
+                newthread.start()
+                self.threads.append(newthread)
+            #for t in self.threads:
+                
+
+            for t in self.threads:
+                t.join()
+            merge.merge(self.sequence)
---
+++ sendtcpmsg.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+import socket
+import urllib.request
+import urllib.response
+import os
+import select
+
+def send_tcp_message(tcpaddress):
+        TCP_IP = tcpaddress
+        TCP_PORT = 9001
+        BUFFER_SIZE = 1024
+        msg="Reporting Client "
+        print(TCP_IP,TCP_PORT)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((TCP_IP,TCP_PORT))
+        s.send(msg.encode())
+        print("tcp conn from client send to server!!!")
+        #msg=s.recv(BUFFER_SIZE)
+        #print (msg)
+        url=s.recv(BUFFER_SIZE)
+        print (url.decode())
+        byte=s.recv(BUFFER_SIZE)
+        print (byte)
+
+        #download starts here
+
+        x=byte.decode()  #this variable might by named x as param to range                                                             #check if recived as string NOT byte
+        url=url.decode()
+        #req = urllib.request.Request(url, headers={'Range':x})
+        #print("file downloading... plz wait")
+        #data = urllib.request.urlopen(req).read()
+        #print("file download complete!!!")
+        #data.encode()
+        #@TODO assign directory path for download
+        downloadFolder = "C://Project/parellel-download/"
+        if not (os.path.isdir("C://Project/parellel-download")):
+            os.makedirs("C://Project/parellel-download/")
+        #downloadPath = downloadFolder + "/" + "temp_file"
+        #f=open(downloadPath,'wb')
+        #print("data is downloded in client side and is abt to write...")
+        
+        
+        #for chunk in data1:   #verify if nessary
+        #        s.sendall(chunk.encode())
+        #f.write(data)
+        #print("data write completed at client side and waiting for server...")
+        #f.close()
+        ###############
+
+        downloadname =str(url.split('/')[-1])#gives proper filename
+        downloadpath=downloadFolder+downloadname
+        print(downloadpath)
+        req = urllib.request.Request(url, headers={'Range':x})
+        #while remaining_download_tries > 0 :
+        print("starting download")
+        try:
+            print("file downloading try 1")
+            #raise HTTPError()
+            with urllib.request.urlopen(url) as fsrc,open(downloadpath,'w+b')as fdst: #NamedTemporaryFile(delete=False) replace open () with Named..() for temp file download
+                copyfileobj(fsrc, fdst,16*1024)
+                print("file downloading complete in try 1")
+                #raise HTTPError()
+        except (HTTPError, URLError) as error:
+            print("file downloading failed ...1.. retrying...")
+            #2
+            try:
+                print("file downloading try 2")
+                with urllib.request.urlopen(inputValue) as fsrc,open(downloadpath,'w+b')as fdst: #NamedTemporaryFile(delete=False) replace open () with Named..() for temp file download
+                    copyfileobj(fsrc, fdst,16*1024)
+                    print("file downloading complete in try 2")
+            except (HTTPError, URLError) as error:
+                print("file downloading failed ...2.. retrying...")
+                #3
+                try:
+                    print("file downloading try 3")
+                    with urllib.request.urlopen(inputValue) as fsrc,open(downloadpath,'w+b')as fdst: #NamedTemporaryFile(delete=False) replace open () with Named..() for temp file download
+                        copyfileobj(fsrc, fdst,16*1024)
+                        print("file downloading complete in try 3")
+                except (HTTPError, URLError) as error:
+                    print("file downloading failed ...3.. aborting...")
+                    print("link expired or not found program is exiting")
+                
+                else:
+                    print("Complete in try3")
+                
+                #3
+            
+            else:
+                print("Complete in try2")
+            
+            #2
+        
+        else:
+            print("Complete in try1")
+        #1
+        ##################
+        #@TODO path :https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
+        
+        write_list=[s]
+        with open(downloadPath,'rb') as f:
+            #d=f.read(4096)
+            #while d:
+            #    s.send(d)
+            #    d=f.read(4096)
+            #    print(d)
+            readable, writable, errored = select.select([],write_list, [])   #check if reciving socket is ready
+            for i in writable:
+                if i is s:
+                    print("file about to send..")
+                    s.sendfile(f)   
+        s.close()
+        print("file send :::")
